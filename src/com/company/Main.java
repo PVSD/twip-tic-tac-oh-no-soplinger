1aed3bccf071860da4f5e4a8ef5ff40d36fd7b1e
soplinger <solekibbles4@gmail.com>
2019-01-03 00:06:23 -0500

    i dont commit anything

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] grid = new String[10];
+        for (int i = 0; i >= 10; i++)
+            grid[i] = "1";
+        Scanner playerOne = new Scanner(System.in);
+        Scanner playerTwo = new Scanner(System.in);
+        int input, winCheck;
+        String checkLine = null;
+        while (true) {
+            System.out.println("Player One input the space you would like to select (0-8)");
+            input = playerOne.nextInt();
+            grid[input] = "X";
+            System.out.println(" " + grid[0] + " | " + grid[1] + " | " + grid[2] + " ");
+            System.out.println("-----------------");
+            System.out.println(" " + grid[3] + " | " + grid[4] + " | " + grid[5] + " ");
+            System.out.println("-----------------");
+            System.out.println(" " + grid[6] + " | " + grid[7] + " | " + grid[8] + " ");
+            System.out.println("-----------------");
+
+            System.out.println("Player Two input the space you would like to select (0-8)");
+            input = playerTwo.nextInt();
+            grid[input] = "O";
+            System.out.println(" " + grid[0] + " | " + grid[1] + " | " + grid[2] + " ");
+            System.out.println("-----------------");
+            System.out.println(" " + grid[3] + " | " + grid[4] + " | " + grid[5] + " ");
+            System.out.println("-----------------");
+            System.out.println(" " + grid[6] + " | " + grid[7] + " | " + grid[8] + " ");
+            System.out.println("-----------------");
+
+        }
+
+
     }
+
 }
